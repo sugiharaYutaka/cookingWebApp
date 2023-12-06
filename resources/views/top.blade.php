31cc80f843d99ff412487d6ef984cd61b1915d6b
sugiharaYutaka <kd1336505@st.kobedenshi.ac.jp>
2023-12-07 00:42:41 +0900

    [add]top controller view

---
+++ resources/views/top.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.header')
+
+<div class="contets-menu body-margin-top">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-6"></div>
+        </div>
+    </div>
+</div>
+
+@extends('layouts.footer')
