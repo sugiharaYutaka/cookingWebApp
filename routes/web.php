183e97ff448d9620ebc07cd4ed9e7676fb2ceeff
Yutaka Sugihara <kd1336505@st.kobedenshi.ac.jp>
2023-12-07 10:00:03 +0900

    [change] web.php

--- routes/web.php
+++ routes/web.php
@@ -30,20 +30,12 @@ Route::get('/study', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-<<<<<<< HEAD
 
-
-// routes/web.php
 use App\Http\Controllers\ChapterController;
 
 Route::get('/chapters', [App\Http\Controllers\ChapterController::class, 'index']);
 Route::post('/chapters', [App\Http\Controllers\ChapterController::class, 'show']);
-=======
-<<<<<<< HEAD
+
 Route::get('/chapter1', [App\Http\Controllers\Chapter1Controller::class, 'index'])->name('chapter1');
-=======
 Route::get('/top', [App\Http\Controllers\TopController::class, 'show']);
 
-
->>>>>>> 2b84d799e603aff16d3ebf9c37e0b4cfdcfbdb74
->>>>>>> f5a2385a2903fc4ad430aa683c136f1212ec1239
