0512e952c75064336fc98d4e106407acd32c6214
Yutaka Sugihara <kd1336505@st.kobedenshi.ac.jp>
2023-12-07 14:41:52 +0900

    [change] migrate user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,12 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('icon_filename')->default('user_icon.png');
             $table->rememberToken();
             $table->timestamps();
         });
