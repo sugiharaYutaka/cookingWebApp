f6ab0ca8cbc38f7f6fe719b8a92b6900ca5ba50d
ko02 <kd1295315@st.kobedenshi.ac.jp>
2023-12-11 14:12:49 +0900

    ゆたか♡

--- routes/web.php
+++ routes/web.php
@@ -63,8 +63,3 @@ Route::get('/profile', function () {
     return 'これは未実装のルートです';
 })->name('profile');
 <<<<<<< HEAD
-
-Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
-Route::post('/post/store', [PostController::class, 'store'])->name('post.store');
-=======
->>>>>>> 415ddeeec5868d225c225c1548468e46abd94795
