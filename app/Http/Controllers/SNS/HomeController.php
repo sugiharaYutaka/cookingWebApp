977230bb86eefd31a3d25f8d0bc5cb9dc5aef7ee
Yutaka Sugihara <kd1336505@st.kobedenshi.ac.jp>
2023-12-11 14:47:16 +0900

    HomeController

--- app/Http/Controllers/SNS/HomeController.php
+++ app/Http/Controllers/SNS/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\SNS;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\SnsPost;
+use App\Models\User;
 
 class HomeController extends Controller
 {
@@ -15,6 +16,16 @@ class HomeController extends Controller
      */
     public function index()
     {
+        User::create([
+            'email' => 'aaa@ggg.com',
+            'name' =>'john doe',
+            'password' => '1234',
+        ]);
+        SnsPost::create([
+            'email' => 'aaa@ggg.com',
+            'text' =>'john doe',
+            'image_filename' => 'dd.png',
+        ]);
 
         // データベースからランダムに5件の投稿を取得
         $randomPosts = SnsPost::inRandomOrder()->limit(5)->get();
